44debcdb27e0f0d39c8eb3600c531a86e7467536
samuel <samuel.cwiek@wp.pl>
2022-10-25 22:58:30 +0200

    commit 4

--- src/main/java/org/example/UserInput.java
+++ src/main/java/org/example/UserInput.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UserInput {
-    Scanner scanner;
+    Scanner scanner = new Scanner(System.in);
     public String checkNameCorrection(){
-        Scanner getP1Name = scanner;
-        String playerInput = getP1Name.nextLine();
+        String playerInput = scanner.nextLine();
         while(playerInput.length() >= 20){
             System.out.println("Name too long");
-            playerInput = getP1Name.nextLine();
+            playerInput = scanner.nextLine();
         }
         return playerInput;
     }
@@ -19,8 +18,8 @@ public class UserInput {
         Scanner submarine = scanner;
         List<String> listOfShips = new ArrayList<>();
         System.out.println("Place your submarine (1 position)");
-        for(int i = 0; i<=1; i++){
-            System.out.println("Place position " + i+1);
+        for(int i = 0; i<=0; i++){
+            System.out.println("Place position " + (i+1));
             listOfShips.add(getValidShipPosition(submarine));
         }
         return listOfShips;
@@ -29,8 +28,8 @@ public class UserInput {
         Scanner boat = scanner;
         List<String> listOfShips = new ArrayList<>();
         System.out.println("Place your boat (2 positions)");
-        for(int i = 0; i<=2; i++){
-            System.out.println("Place position " + i+1);
+        for(int i = 0; i<=1; i++){
+            System.out.println("Place position " + (i+1));
             listOfShips.add(getValidShipPosition(boat));
         }
         return listOfShips;
@@ -39,8 +38,8 @@ public class UserInput {
         Scanner destroyer = scanner;
         List<String> listOfShips = new ArrayList<>();
         System.out.println("Place your destroyer (3 positions)");
-        for(int i = 0; i<=3; i++){
-            System.out.println("Place position " + i+1);
+        for(int i = 0; i<=2; i++){
+            System.out.println("Place position " + (i+1));
             listOfShips.add(getValidShipPosition(destroyer));
         }
         return listOfShips;
@@ -49,8 +48,8 @@ public class UserInput {
         Scanner carrier = scanner;
         List<String> listOfShips = new ArrayList<>();
         System.out.println("Place your carrier (4 positions)");
-        for(int i = 0; i<=4; i++){
-            System.out.println("Place position " + i+1);
+        for(int i = 0; i<=3; i++){
+            System.out.println("Place position " + (i+1));
             listOfShips.add(getValidShipPosition(carrier));
         }
         return listOfShips;
@@ -59,26 +58,25 @@ public class UserInput {
         Scanner cargoShip = scanner;
         List<String> listOfShips = new ArrayList<>();
         System.out.println("Place your cargo ship (5 positions)");
-        for(int i = 0; i<=5; i++){
-            System.out.println("Place position " + i+1);
+        for(int i = 0; i<=4; i++){
+            System.out.println("Place position " + (i+1));
             listOfShips.add(getValidShipPosition(cargoShip));
         }
         return listOfShips;
     }
     public String getValidShipPosition (Scanner shipScanner){
         String shipPosition = shipScanner.nextLine();
-        char letter = shipPosition.toCharArray()[0];
+        char letter = shipPosition.toLowerCase().toCharArray()[0];
         char number = shipPosition.toCharArray()[1];
-        while(shipPosition.length() == 2){
+        while(shipPosition.length() != 2){
             System.out.println("Wrong position length");
             shipPosition = shipScanner.nextLine();
         }
-        while(letter <= 'a' || letter >= 'i'||number <= '1' || number >= '9'){
+        while(letter < 'a' || letter > 'i' && number < '1' || number > '9'){
             System.out.println("Wrong ship coordinate, place ship with correct pattern (A1)");
             shipPosition = shipScanner.nextLine();
             letter = shipPosition.toCharArray()[0];
             number = shipPosition.toCharArray()[1];
-
         }
         return shipPosition;
     }
